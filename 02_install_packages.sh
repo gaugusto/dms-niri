eb5fdba0c8d60b43f617b9e09171d9fdfe6b5acd
Guilherme Augusto de Macedo <gaugustomacedo@gmail.com>
2026-04-02 15:47:12 -0300

    pakages list updated

--- 02_install_packages.sh
+++ 02_install_packages.sh
@@ -4,38 +4,46 @@
 set -e
 
 PACKAGES=(
-adw-gtk-theme
-base
-bat
-bluez-utils
-bob
-catppuccin-cursors-mocha
-cliphist
-cups
-dms-shell-bin
-efibootmgr
-eza
-firefox
-fzf
-greetd-dms-greeter-git
-intel-ucode
-lazygit
-limine
-linux-zen
-mpv
-nwg-look
-pavucontrol
-plymouth
-polkit-gnome
-snapper
-sof-firmware
-stow
-system-config-printer
-ttf-jetbrains-mono-nerd
-walker
-yay
-yay-debug
-zram-generator
+  adw-gtk-theme
+  base
+  bat
+  bluez-utils
+  bob
+  catppuccin-cursors-mocha
+  clang
+  cliphist
+  cups
+  dms-shell-bin
+  efibootmgr
+  eza
+  fd
+  firefox
+  fzf
+  greetd-dms-greeter-git
+  intel-ucode
+  lazygit
+  libva-utils
+  limine
+  linux-zen
+  lua-argparse
+  lua-jsregexp
+  luarocks
+  mpv
+  nwg-look
+  pavucontrol
+  plymouth
+  polkit-gnome
+  ripgrep
+  snapper
+  sof-firmware
+  stow
+  system-config-printer
+  tree
+  ttf-jetbrains-mono-nerd
+  walker
+  yay
+  yay-debug
+  zram-generator
 )
 
 sudo pacman -Syy
