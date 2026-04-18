085e9ac989ee274c89f41123187f119378065f43
Guilherme Augusto de Macedo <gaugustomacedo@gmail.com>
2026-04-18 16:56:31 -0300

    changing aliases

--- aliases/.oh-my-zsh/custom/aliases.zsh
+++ aliases/.oh-my-zsh/custom/aliases.zsh
@@ -1,8 +1,9 @@
 alias fzf='fzf --height 80% --layout reverse --border'
 
-alias pinfo="pacman -Qq | fzf --preview 'pacman -Qi {}'"
-alias pinstall="pacman -Sl | awk '{print \$2}' | fzf -m --preview 'pacman -Si {}' --layout=reverse | xargs -ro sudo pacman --needed -S"
-alias puninstall="pacman -Qq | fzf -m --preview 'pacman -Si {}' --layout=reverse | xargs -ro sudo pacman -Rsc"
+alias pacman-info="pacman -Qq | fzf --preview 'pacman -Qi {}'"
+alias pacman-install="pacman -Sl | awk '{print \$2}' | fzf -m --preview 'pacman -Si {}' --layout=reverse | xargs -ro sudo pacman --needed -S"
+alias yay-install="yay -Sl | awk '{print \$2}' | fzf -m --preview 'yay -Si {}' --layout=reverse | xargs -ro yay --needed -S"
+alias pacman-uninstall="pacman -Qq | fzf -m --preview 'pacman -Si {}' --layout=reverse | xargs -ro sudo pacman -Rsc"
 
 alias me-btw="figlet GAugusto"
 
